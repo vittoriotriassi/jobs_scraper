8a0648390ba6bff81b1aba7527791b09cd005aca
Vittorio Triassi <34443219+vittoriotriassi@users.noreply.github.com>
2020-09-23 20:37:44 +0200

    Added test_scraping.py

---
+++ tests/test_scraping.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+import pytest
+from jobs_scraper.scraping import JobsScraper
+import pandas as pd
+
+
+class TestScraping(TestCase):
+
+    def setUp(self):
+        self._scraper = JobsScraper(headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'},
+                                    url='https://www.indeed.nl/vacatures?q=software+engineer&l=', pages=3)
+
+
+    def test_scraping(self):
+        
+        self._scraper.scrape()
+        
