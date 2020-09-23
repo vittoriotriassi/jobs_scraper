ed7c945414748d34e46062695a2666ca7602367e
Vittorio Triassi <34443219+vittoriotriassi@users.noreply.github.com>
2020-09-23 20:37:31 +0200

    Updated setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+test_deps = [
+    "pytest",
+    "pytest-pylint",
+    "pytest-cov"
+]
+
+setup(
+    name='jobs_scraper',
+    version='1.0',
+    description="A simple job postings scraper for Indeed.",
+    packages=find_packages(),
+    tests_require=test_deps,
+    # Add here the package dependencies
+    install_requires=[
+        'pandas',
+    ],
+)
