b94ec877ee8ad3ddfeae9c13f83f0c2b48c45a1b
Vittorio Triassi <34443219+vittoriotriassi@users.noreply.github.com>
2020-09-25 01:44:46 +0200

    Updated scraping.py

--- jobs_scraper/scraping.py
+++ jobs_scraper/scraping.py
@@ -1,10 +1,13 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from itertools import cycle
 from fake_useragent import UserAgent
+from tqdm import tqdm
 
 class JobsScraper:
     """JobsScraper is a simple job postings scraper for Indeed."""
+
     def __init__(self, url: str, pages: int):
         """
         Create a JobsScraper object.
@@ -22,46 +25,71 @@ class JobsScraper:
         self._ua = UserAgent()
         self._headers = {'User-Agent': self._ua.random}
         self._pages = pages
+        self._proxies = {}
         self._jobs = []
-        
+
+    def _proxies_pool(self):
+
+        url = 'https://www.sslproxies.org/'
+
+        r = requests.get(url, headers=self._headers)
+
+        df = pd.read_html(r.text)
+
+        df[0] = df[0].dropna()
+        self._proxies = df[0].set_index('IP Address')['Port'].astype(int).to_dict()
+
     def _extract_page(self, page):
 
-        r = requests.get("{}&start={}".format(self._url, page), headers = self._headers)
+        proxies = cycle(self._proxies)
+
+        current_proxy = next(proxies)
+
+        with requests.Session() as request:
+            r = request.get(url="{}&start={}".format(self._url, page), proxies={
+                            "http://": current_proxy, "https://": current_proxy}, headers=self._headers, timeout=30)
+
         soup = BeautifulSoup(r.content, 'html.parser')
 
         return soup
 
     def _transform_page(self, soup):
 
-        jobs = soup.find_all('div', class_ = 'jobsearch-SerpJobCard')
+        jobs = soup.find_all('div', class_='jobsearch-SerpJobCard')
 
         for job in jobs:
 
             try:
-                title = job.find('a', class_ = 'jobtitle').text.strip().replace('\n', '')
+                title = job.find(
+                    'a', class_='jobtitle').text.strip().replace('\n', '')
             except:
                 title = None
             try:
-                company = job.find('span', class_ = 'company').text.strip().replace('\n', '')
+                company = job.find(
+                    'span', class_='company').text.strip().replace('\n', '')
             except:
                 company = None
             try:
-                summary = job.find('div', {'class':'summary'}).text.strip().replace('\n', '')
+                summary = job.find(
+                    'div', {'class': 'summary'}).text.strip().replace('\n', '')
             except:
                 summary = None
 
-            if job.find('div', class_ = 'location'):
+            if job.find('div', class_='location'):
                 try:
-                    location = job.find('div', class_ = 'location').text.strip().replace('\n', '')
+                    location = job.find(
+                        'div', class_='location').text.strip().replace('\n', '')
                 except:
                     location = None
             else:
                 try:
-                    location = job.find('span', class_ = 'location').text.strip().replace('\n', '')
+                    location = job.find(
+                        'span', class_='location').text.strip().replace('\n', '')
                 except:
                     location = None
             try:
-                salary = job.find('span', class_ = 'salary').text.strip().replace('\n', '')
+                salary = job.find(
+                    'span', class_='salary').text.strip().replace('\n', '')
             except:
                 salary = None
 
@@ -86,11 +114,13 @@ class JobsScraper:
             Return a scraped Dataframe.
         """
 
-        for i in range(0, self._pages * 10, 10):
+        self._proxies_pool()
+
+        for i in tqdm(range(0, self._pages * 10, 10), desc = "Scraping in progress...", total = self._pages):
 
             page = self._extract_page(i)
             self._transform_page(page)
-        
+
         df = pd.DataFrame(self._jobs)
         df.drop_duplicates(inplace=True)
 
