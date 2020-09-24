0fb9de6d202e54a6aa5fd6412e465e7488db8a79
Vittorio Triassi <34443219+vittoriotriassi@users.noreply.github.com>
2020-09-24 12:12:09 +0200

    Updated test

--- tests/test_scraping.py
+++ tests/test_scraping.py
@@ -7,8 +7,8 @@ import pandas as pd
 class TestScraping(TestCase):
     """Test to validate JobsScraper."""
     def setUp(self):
-        self._scraper = JobsScraper(headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'},
-                                    url='https://www.indeed.nl/vacatures?q=software+engineer&l=', pages=3)
+        
+        self._scraper = JobsScraper(url='https://www.indeed.nl/vacatures?q=software+engineer&l=', pages=3)
 
 
     def test_scraping(self):
