39d5292d3f4fb6da20957be131537e4f5e2714dc
Vittorio Triassi <34443219+vittoriotriassi@users.noreply.github.com>
2020-09-25 14:23:38 +0200

    Added rotating proxies

--- jobs_scraper/scraping.py
+++ jobs_scraper/scraping.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+from time import sleep
+import random
 from bs4 import BeautifulSoup
 from itertools import cycle
 from fake_useragent import UserAgent
@@ -8,7 +10,7 @@ from tqdm import tqdm
 class JobsScraper:
     """JobsScraper is a simple job postings scraper for Indeed."""
 
-    def __init__(self, url: str, pages: int):
+    def __init__(self, url: str, pages: int, max_delay = 1):
         """
         Create a JobsScraper object.
 
@@ -18,32 +20,34 @@ class JobsScraper:
             Url to be scraped.
             Please provide the general query from the Indeed homepage.
         pages: int
-            Number of pages to be scraped.
-            Each page contains 15 results.
+            Number of pages to be scraped. Each page contains 15 results.
+        max_delay = 1 (Optional)
+            Max number of seconds of delay for the scraping of a single posting.
         """
         self._url = url
         self._ua = UserAgent()
         self._headers = {'User-Agent': self._ua.random}
         self._pages = pages
-        self._proxies = {}
+        self._proxy = []
+        self._max_delay = max_delay
         self._jobs = []
 
-    def _proxies_pool(self):
+    def _proxies(self):
 
         url = 'https://www.sslproxies.org/'
 
         r = requests.get(url, headers=self._headers)
 
         df = pd.read_html(r.text)
-
-        df[0] = df[0].dropna()
-        self._proxies = df[0].set_index('IP Address')['Port'].astype(int).to_dict()
+        df[0].dropna(inplace=True)
+        df[0]['Port'] = df[0]['Port'].astype(int).astype(str)
+        df[0]['IP_Port'] = df[0]['IP Address'].str.cat(df[0]['Port'], sep=":")
+        proxies_pool = df[0]['IP_Port'].tolist()
+        self._proxy = cycle(proxies_pool)
 
     def _extract_page(self, page):
 
-        proxies = cycle(self._proxies)
-
-        current_proxy = next(proxies)
+        current_proxy = next(self._proxy)
 
         with requests.Session() as request:
             r = request.get(url="{}&start={}".format(self._url, page), proxies={
@@ -103,6 +107,15 @@ class JobsScraper:
 
             self._jobs.append(job)
 
+            print("Scraping {}...".format(title))
+
+            if self._max_delay > 1:
+                sleep(random.randint(1, self._max_delay))
+            else:
+                sleep(1)
+            
+            
+
     def scrape(self) -> pd.DataFrame:
         """
         Perform the scraping of the url provided in the class constructor.
@@ -114,7 +127,7 @@ class JobsScraper:
             Return a scraped Dataframe.
         """
 
-        self._proxies_pool()
+        self._proxies()
 
         for i in tqdm(range(0, self._pages * 10, 10), desc = "Scraping in progress...", total = self._pages):
 
