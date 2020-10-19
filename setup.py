c8d513bc93a55bbb71ac958149bd78a244bae7b3
Vittorio Triassi <34443219+vittoriotriassi@users.noreply.github.com>
2020-10-19 02:29:07 +0200

    Added new version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ classifiers = [
 
 setup(
     name='jobs_scraper',
-    version='0.0.9',
+    version='0.0.10',
     description="A simple job postings scraper for Indeed based on requests and BeautifulSoup.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
